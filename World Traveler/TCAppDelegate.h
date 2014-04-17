92e89de4ce7b72ad3ed82cdf8b924f530270ae2e
razvizion <kolo111@gmail.com>
2014-04-18 00:20:12 +0200

    Initial Commit

---
+++ World Traveler/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  World Traveler
+//
+//  Created by Michał Kozak on 18.04.2014.
+//  Copyright (c) 2014 Raz Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ World Traveler/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  World Traveler
+//
+//  Created by Michał Kozak on 18.04.2014.
+//  Copyright (c) 2014 Raz Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+    }
+}
